e196f3f279cdedf0ee9400ad2d8f00c2ee2b1940
Nicksol <yniluka@gmail.com>
2014-12-12 22:28:26 +0530

    version 1.4
    GUI Version

--- NetCQ/src/netcq/extract.java
+++ NetCQ/src/netcq/extract.java
@@ -1,8 +1,8 @@
 /*
  * extract.java
- * Queries for extracting data  
- * Version 1.3
- * 08/12/2014
+ * Getcommon values of two lists  
+ * Version 1.4
+ * 12/12/2014
  */
 
 package netcq;
@@ -10,75 +10,34 @@ package netcq;
 import java.util.*;
 
 public class extract {
-    
-    public static List<Integer> getpacketnumber(HashMap<String,List<Integer>> hname,String ipadd){
-    
-        List<Integer> numlist= hname.get(ipadd);;
-        for(int i=0;i<numlist.size();i++){
-            output.printpacket(numlist.get(i));
-            //System.out.println(numlist);
-        }
-    
-        return numlist;
-    }
-    
-    
-    // return packet num list for any port
-    public static List<Integer> getpacketnumberport(HashMap<Integer,List<Integer>> hname,Integer portnd){
-    
-        List<Integer> numlist= hname.get(portnd);;
-        System.out.println(numlist);
-        return numlist;
-    }
-
-
-    // compare the src ip and the destination ip and get the list
-    public static List<Integer> getpacketsrcdesc(String srcip,String descip){
-    
-        List<Integer> srclist= Data.srcip_map.get(srcip);
-        List<Integer> desclist= Data.destip_map.get(descip);
-        List<Integer> retlist = new ArrayList<Integer>();
-        int i,j;
-        for( i=0;i<srclist.size();i++){
-            for(j=0;j<desclist.size();j++){
-                if(srclist.get(i)==desclist.get(j)){
-                    output.printpacket(srclist.get(i));
-                    retlist.add(srclist.get(i)); 
-                }
-            }
-        }
-        return retlist;
-    }
-
 
     // compare two lists and return the common item list;
-    public static List<Integer> getcommon(List<Integer> lista,List<Integer> listb){
-        List<Integer> retlist = new ArrayList<Integer>();
-        int i,j;
-        for( i=0;i<lista.size();i++){
-            for(j=0;j<listb.size();j++){
-                if(lista.get(i)==listb.get(j)){
-                    output.printpacket(lista.get(i));
-                    retlist.add(lista.get(i)); 
+    public static List<Integer> getcommon(List<Integer> lista, List<Integer> listb) {
+        List<Integer> retlist1 = new ArrayList<Integer>();
+        List<Integer> retlist2 = new ArrayList<Integer>();
+        if (lista != null && listb != null) {
+            for (int n = 0; n < lista.size(); n++) {
+                if (listb.contains(lista.get(n))) {
+                    retlist1.add(lista.get(n));
                 }
             }
-        }
-    
-        return retlist;
-    }
-    
-    //length of packets which are transfer through specific port
-    
-    public static List<Integer> getpacketlength(HashMap<Integer, List<Integer>> srcport_map,int port){
-		List<Integer> numlist= srcport_map.get(port);;
-		
-		for(int i=0;i<numlist.size();i++){
-			output.printpacketlngth(numlist.get(i));
-			}
-			
-		return numlist;
-    }
-     
-}
-    
+            Iterator<Integer> itr1 = retlist1.listIterator();
+            while (itr1.hasNext()) {
+                Integer itn1 = (Integer) itr1.next();
+                if (retlist2.contains(itn1) == false) {
+                    retlist2.add(itn1);
+                }
+            }
+        } else if (lista != null || listb != null) {
+            if (lista != null) {
+                retlist2 = lista;
+            }
+            if (listb != null) {
+                retlist2 = listb;
 
+            }
+        }
+        return retlist2;
+    }
+
+}
--- NetCQ/src/netcq/mapStore.java
+++ NetCQ/src/netcq/mapStore.java
@@ -1,250 +1,241 @@
 /*
  * mapStore.java
  * Define MultivaluedMaps to store details of packets 
- * Version 1.3
- * 08/12/2014
+ * Version 1.4
+ * 12/12/2014
  */
-
 
 package netcq;
 
 import java.util.*;
 import jpcap.packet.*;
 
-
 public class mapStore {
-    
+
     //convert captured Packet object to PacketData object and find the protocol of the packet
-    public static PacketData converting(Packet cap_packet){
-         
+    public static PacketData converting(Packet cap_packet) {
+
         PacketData pack = new PacketData();
-         
-         if(cap_packet instanceof TCPPacket && (cap_packet instanceof IPPacket) ){
-		TCPPacket tc = (TCPPacket) cap_packet;
-                pack.length1 = (int)tc.length;
-                pack.src_ip1 = tc.src_ip.getHostAddress();
-                pack.src_port1 = tc.src_port;
-                pack.dest_ip1 = tc.dst_ip.getHostAddress();
-                pack.dest_port1 = tc.dst_port;
-                pack.protocol1.add("IP");
-                pack.protocol1.add("TCP");
-                if (tc.src_port == 8080)
-                    pack.protocol1.add("HTTP");   
-                else if(tc.src_port == 21)
-                    pack.protocol1.add("FTP");
-                else if(tc.src_port == 23)
-                    pack.protocol1.add("Telnet");
-                else if(tc.src_port == 25)
-                    pack.protocol1.add("SMTP");
-                else
-                    pack.protocol1.add("TCP");
-                
-         }
-           
-        if(cap_packet instanceof UDPPacket && cap_packet instanceof IPPacket){
-		UDPPacket ud = (UDPPacket) cap_packet;
-		pack.length1 = ud.length;
-                pack.src_ip1 = ud.src_ip.getHostAddress();
-                pack.src_port1 = ud.src_port;
-                pack.dest_ip1 = ud.dst_ip.getHostAddress();
-                pack.dest_port1 = ud.dst_port;
-                pack.protocol1.add("IP");
-                pack.protocol1.add("TCP");
-                if(ud.src_port == 53)
-                    pack.protocol1.add("DNS");
-                else if(ud.src_port == 137)
-                    pack.protocol1.add("NBNS");
-                else
-                    pack.protocol1.add("UDP");
-                
-             
+
+        if (cap_packet instanceof TCPPacket && (cap_packet instanceof IPPacket)) {
+            TCPPacket tc = (TCPPacket) cap_packet;
+            pack.length1 = (int) tc.length;
+            pack.src_ip1 = tc.src_ip.getHostAddress();
+            pack.src_port1 = tc.src_port;
+            pack.dest_ip1 = tc.dst_ip.getHostAddress();
+            pack.dest_port1 = tc.dst_port;
+            pack.protocol1.add("IP");   //Network layer
+            pack.protocol1.add("TCP");  //Transport layer
+            if (tc.src_port == 8080) {
+                pack.protocol1.add("HTTP");
+            } else if (tc.src_port == 21) {
+                pack.protocol1.add("FTP");
+            } else if (tc.src_port == 23) {
+                pack.protocol1.add("Telnet");
+            } else if (tc.src_port == 25) {
+                pack.protocol1.add("SMTP");
+            } else {
+                pack.protocol1.add("T");    //Other packets in application layer(TCP in Transport layer)
+            }
+
         }
-       
-        if(cap_packet instanceof ICMPPacket && cap_packet instanceof IPPacket){
-		ICMPPacket icmp = (ICMPPacket) cap_packet;
-		pack.length1 = (int)icmp.length;
-                pack.src_ip1 = icmp.src_ip.getHostAddress();
-                pack.src_port1 = 99999;
-                pack.dest_ip1 = icmp.dst_ip.getHostAddress();
-                pack.dest_port1 = 99999;
-                pack.protocol1.add("ICMP");
-                
+
+        if (cap_packet instanceof UDPPacket && cap_packet instanceof IPPacket) {
+            UDPPacket ud = (UDPPacket) cap_packet;
+            pack.length1 = ud.length;
+            pack.src_ip1 = ud.src_ip.getHostAddress();
+            pack.src_port1 = ud.src_port;
+            pack.dest_ip1 = ud.dst_ip.getHostAddress();
+            pack.dest_port1 = ud.dst_port;
+            pack.protocol1.add("IP");   //Network layer
+            pack.protocol1.add("UDP");  //Transport layer
+            if (ud.src_port == 53) {
+                pack.protocol1.add("DNS");
+            } else if (ud.src_port == 137) {
+                pack.protocol1.add("NBNS");
+            } else {
+                pack.protocol1.add("U");    //Other packets in application layer(UDP in Transport layer)
+            }
+
         }
-        
-        return pack;    
+
+        if (cap_packet instanceof ICMPPacket && cap_packet instanceof IPPacket) {
+            ICMPPacket icmp = (ICMPPacket) cap_packet;
+            pack.length1 = (int) icmp.length;
+            pack.src_ip1 = icmp.src_ip.getHostAddress();
+            pack.src_port1 = 99999; //Unused port number to indicate no src port for ICMP
+            pack.dest_ip1 = icmp.dst_ip.getHostAddress();
+            pack.dest_port1 = 99999;    //Unused port number to indicate no dest port for ICMP
+            pack.protocol1.add("ICMP"); //Network layer
+
+        }
+
+        return pack;
     }
 
-    
     //method to insert packetnumbers list to a specific src_ip by checking srcip_map hashmap contains that src_ip
-    public static void addsrcip(PacketData packet,Data data,String src,int count1,HashMap srcip_tempmap){
-        
-         if(!Data.srcip_map.containsKey(packet.src_ip1)){
-            
-            List<Integer> srcip_count = new ArrayList<> ();
+    public static void addsrcip(PacketData packet, Data data, String src, int count1) {
+
+        if (!Data.srcip_map.containsKey(packet.src_ip1)) {
+
+            List<Integer> srcip_count = new ArrayList<>();
             srcip_count.add(count1);
-            Data.srcip_map.put(packet.src_ip1,srcip_count);
-            }
-            else{
-                List<Integer> temp = new ArrayList<> ();
-                String tempsrc = Data.alldata_map.get(count1).src_ip1;
-                if(srcip_tempmap.containsKey(src)){
-                
-                    for ( String key : Data.srcip_map.keySet() ) {
-                    
-                        if(tempsrc.equals(key)){
-                            
-                            temp = Data.srcip_map.get(src);
-                            srcip_tempmap.put(src ,temp.add(count1));
-                            Data.srcip_map.put(packet.src_ip1,temp);
-                        }
+            Data.srcip_map.put(packet.src_ip1, srcip_count);
+        } else {
+            List<Integer> temp = new ArrayList<>();
+            String tempsrc = Data.alldata_map.get(count1).src_ip1;
+            if (Data.srcip_map.containsKey(src)) {
+
+                for (String key : Data.srcip_map.keySet()) {
+
+                    if (tempsrc.equals(key)) {
+
+                        temp = Data.srcip_map.get(src);
+                        temp.add(count1);
+                        Data.srcip_map.put(src,temp );
+                        Data.srcip_map.put(packet.src_ip1, temp);
                     }
                 }
             }
         }
-    
-    
+    }
+
     //method to insert packetnumbers list to a specific dest_ip by checking destip_map hashmap contains that dest_ip
-    public static void adddestip(PacketData packet,Data data,String dest,int count1,HashMap destip_tempmap){
-        
-         if(!Data.destip_map.containsKey(packet.dest_ip1)){
-            
-            List<Integer> destip_count = new ArrayList<> ();
+    public static void adddestip(PacketData packet, Data data, String dest, int count1) {
+
+        if (!Data.destip_map.containsKey(packet.dest_ip1)) {
+
+            List<Integer> destip_count = new ArrayList<>();
             destip_count.add(count1);
-            Data.destip_map.put(packet.dest_ip1,destip_count);
-            }
-            else{
-                List<Integer> temp = new ArrayList<> ();
-                String tempsrc = Data.alldata_map.get(count1).dest_ip1;
-                if(destip_tempmap.containsKey(dest)){
-                    for ( String key : Data.destip_map.keySet() ) {
-                    
-                        if(tempsrc.equals(key)){
-                            
-                            temp = Data.destip_map.get(dest);
-                            destip_tempmap.put(dest ,temp.add(count1));
-                            Data.destip_map.put(packet.dest_ip1,temp);
-                        }
+            Data.destip_map.put(packet.dest_ip1, destip_count);
+        } else {
+            List<Integer> temp = new ArrayList<>();
+            String tempsrc = Data.alldata_map.get(count1).dest_ip1;
+            if (Data.destip_map.containsKey(dest)) {
+                for (String key : Data.destip_map.keySet()) {
+
+                    if (tempsrc.equals(key)) {
+
+                        temp = Data.destip_map.get(dest);
+                        temp.add(count1);
+                        Data.destip_map.put(dest, temp);
+                        Data.destip_map.put(packet.dest_ip1, temp);
                     }
                 }
             }
         }
-    
-    
+    }
+
     //method to insert packetnumbers list to a specific src_port by checking srcport_map hashmap contains that src_port
-    public static void addsrcport(PacketData packet,Data data,int port,int count1,HashMap srcport_tempmap){
-        
-         if(!Data.srcport_map.containsKey(packet.src_port1)){
-            
-            List<Integer> srcport_count = new ArrayList<> ();
-            //System.out.println("newsrcport");
+    public static void addsrcport(PacketData packet, Data data, int port, int count1) {
+
+        if (!Data.srcport_map.containsKey(packet.src_port1)) {
+
+            List<Integer> srcport_count = new ArrayList<>();
             srcport_count.add(count1);
-            Data.srcport_map.put(packet.src_port1,srcport_count);
-            }
-            else{
-               
-                List<Integer> temp = new ArrayList<> ();
-                int tempport = Data.alldata_map.get(count1).src_port1;
-                if(srcport_tempmap.containsKey(port)){
-                     //System.out.println("oldsrcport");
-                    for ( int key : Data.srcport_map.keySet() ) {
-                    
-                        if(tempport == key){
-                            
-                            temp = Data.srcport_map.get(port);
-                            srcport_tempmap.put(port ,temp.add(count1));
-                            Data.srcport_map.put(packet.src_port1,temp);
-                        }
+            Data.srcport_map.put(packet.src_port1, srcport_count);
+        } else {
+
+            List<Integer> temp = new ArrayList<>();
+            int tempport = Data.alldata_map.get(count1).src_port1;
+            if (Data.srcport_map.containsKey(port)) {
+
+                for (int key : Data.srcport_map.keySet()) {
+
+                    if (tempport == key) {
+
+                        temp = Data.srcport_map.get(port);
+                        temp.add(count1);
+                        Data.srcport_map.put(port, temp);
+                        Data.srcport_map.put(packet.src_port1, temp);
                     }
                 }
             }
         }
-    
-    
+    }
+
     //method to insert packetnumbers list to a specific dest_port by checking destport_map hashmap contains that dest_port
-    public static void adddestport(PacketData packet,Data data,int port,int count1,HashMap destport_tempmap){
-        
-         if(!Data.destport_map.containsKey(packet.dest_port1)){
-            
-            List<Integer> destport_count = new ArrayList<> ();
+    public static void adddestport(PacketData packet, Data data, int port, int count1) {
+
+        if (!Data.destport_map.containsKey(packet.dest_port1)) {
+
+            List<Integer> destport_count = new ArrayList<>();
             destport_count.add(count1);
-            Data.destport_map.put(packet.dest_port1,destport_count);
+            Data.destport_map.put(packet.dest_port1, destport_count);
+        } else {
+            List<Integer> temp = new ArrayList<>();
+            int tempport = Data.alldata_map.get(count1).dest_port1;
+            if (Data.destport_map.containsKey(port)) {
+
+                for (int key : Data.destport_map.keySet()) {
+
+                    if (tempport == key) {
+
+                        temp = Data.destport_map.get(port);
+                        temp.add(count1);
+                        Data.destport_map.put(port, temp);
+                        Data.destport_map.put(packet.dest_port1, temp);
+                    }
+                }
             }
-            else{
-                List<Integer> temp = new ArrayList<> ();
-                int tempport = Data.alldata_map.get(count1).dest_port1;
-                if(destport_tempmap.containsKey(port)){
-                
-                    for ( int key : Data.destport_map.keySet() ) {
-                    
-                        if(tempport == key){
-                            
-                            temp = Data.destport_map.get(port);
-                            destport_tempmap.put(port ,temp.add(count1));
-                            Data.destport_map.put(packet.dest_port1,temp);
+        }
+    }
+
+    //method to insert packetnumbers list to a specific length by checking length_map hashmap contains that length value
+    public static void addlength(PacketData packet, Data data, int length, int count1) {
+
+        if (!Data.length_map.containsKey(packet.length1)) {
+
+            List<Integer> length_count = new ArrayList<>();
+            length_count.add(count1);
+            Data.length_map.put(packet.length1, length_count);
+        } else {
+            List<Integer> temp = new ArrayList<>();
+            int templength = Data.alldata_map.get(count1).length1;
+            if (Data.length_map.containsKey(length)) {
+
+                for (int key : Data.length_map.keySet()) {
+
+                    if (templength == key) {
+
+                        temp = Data.length_map.get(length);
+                        temp.add(count1);
+                        Data.length_map.put(length, temp);
+                        Data.length_map.put(packet.length1, temp);
+                    }
+                }
+            }
+        }
+    }
+
+    //method to insert packetnumbers list to a specific protocol by checking protocol_map hashmap contains that protocol value
+    public static void addprotocol(PacketData packet, Data data, List<String> protocol, int count1) {
+
+        int protocols = packet.protocol1.size();
+
+        for (int i = 0; i < protocols; i++) {
+            if (!Data.protocol_map.containsKey(packet.protocol1.get(i))) {
+                List<Integer> protocol_count = new ArrayList<>();
+                protocol_count.add(count1);
+                Data.protocol_map.put(packet.protocol1.get(i), protocol_count);
+
+            } else {
+                List<Integer> temp = new ArrayList<>();
+                String tempprotocol = Data.alldata_map.get(count1).protocol1.get(i);
+
+                if (Data.protocol_map.containsKey(tempprotocol)) {
+                 
+                    for (String key : Data.protocol_map.keySet()) {
+
+                        if (tempprotocol.equals(key)) {
+                            temp = Data.protocol_map.get(tempprotocol);
+                            temp.add(count1);
+                            Data.protocol_map.put(packet.protocol1.get(i), temp);
                         }
                     }
                 }
             }
         }
-    
-    
-    //method to insert packetnumbers list to a specific length by checking length_map hashmap contains that length value
-    public static void addlength(PacketData packet,Data data,int length,int count1,HashMap length_tempmap){
-        
-         if(!Data.length_map.containsKey(packet.length1)){
-            
-            List<Integer> length_count = new ArrayList<> ();
-            length_count.add(count1);
-            Data.length_map.put(packet.length1,length_count);
-            }
-            else{
-                List<Integer> temp = new ArrayList<> ();
-                int templength = Data.alldata_map.get(count1).length1;
-                if(length_tempmap.containsKey(length)){
-                
-                    for ( int key : Data.length_map.keySet() ) {
-                    
-                        if(templength == key){
-                            
-                            temp = Data.length_map.get(length);
-                            length_tempmap.put(length ,temp.add(count1));
-                            Data.length_map.put(packet.length1,temp);
-                        }
-                    }
-                }
-            }
     }
     
-    
-    //method to insert packetnumbers list to a specific protocol by checking protocol_map hashmap contains that protocol value
-    public static void addprotocol(PacketData packet,Data data,List<String> protocol,int count1,HashMap protocol_tempmap){
-         
-         int protocols = packet.protocol1.size();
-         for(int i=0;i<protocols;i++){
-         if(!Data.protocol_map.containsKey(packet.protocol1.get(i))){
-            
-            List<Integer> protocol_count = new ArrayList<> ();
-            protocol_count.add(count1);
-            Data.protocol_map.put(packet.protocol1.get(i),protocol_count);
-            }
-            else{
-                List<Integer> temp = new ArrayList<> ();
-                String tempprotocol = Data.alldata_map.get(count1).protocol1.get(i);
-                if(protocol_tempmap.containsKey(protocol)){
-                
-                    for ( String key : Data.protocol_map.keySet() ) {
-                    
-                        if(tempprotocol.equals(key)){
-                            
-                            temp = Data.protocol_map.get(protocol);
-                            protocol_tempmap.put(protocol ,temp.add(count1));
-                            Data.protocol_map.put(packet.protocol1.get(i),temp);
-                        }
-                    }
-                }
-            }
-         }
-    }
-
 }
-
